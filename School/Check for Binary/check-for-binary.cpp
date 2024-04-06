930c690aa0de19dfbac37098923b577a0c793dfc
Sneha Agrawal <119348845+snehaagrawal14@users.noreply.github.com>
2024-04-06 18:39:00 +0530

    Added Solution - GfG to GitHub

---
+++ School/Check for Binary/check-for-binary.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// C++ program to Print root to leaf path WITHOUT
+// using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isBinary(string str);
+
+// Driver program to test above functions
+int main()
+{
+    string str;
+    int t;
+    scanf("%d\n", &t);
+    while (t--)
+    {
+       cin >> str;
+       cout << isBinary(str) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+// Return true if str is binary, else false
+bool isBinary(string str)
+{
+   
+   for(auto i: str){
+       if(i!='0' && i!='1'){
+           return false;
+       }
+   }
+   return true;
+   
+}
